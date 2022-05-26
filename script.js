be434202fe2753b9c4e79f7bad017838cb2039b1
Kasper Nikolajsen Dahl <kasp103y@stud.kea.dk>
2022-05-26 16:02:52 +0200

    notice væk på scroll

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ const burgerContent = document.querySelector(".burger-content");
 const noticeMeSenpai = document.querySelector(".notice");
 
 burger.addEventListener("click", showBurger);
+window.addEventListener("scroll", removeArrow);
 
 function showBurger() {
   burger.classList.toggle("open");
   burgerContent.classList.toggle("open");
+}
+
+function removeArrow() {
+  noticeMeSenpai;
+  noticeMeSenpai.classList.toggle("hide", window.scrollY > 0);
+  console.log("hej");
 }
