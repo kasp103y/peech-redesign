f869254927df67655f674495876ca0478e1f6b79
Kasper Nikolajsen Dahl <kasp103y@stud.kea.dk>
2022-06-01 17:41:13 +0200

    kdkd

--- cookie.js
+++ cookie.js
@@ -1,18 +1,9 @@
-const burger = document.querySelector(".hamburger");
-const burgerContent = document.querySelector(".burger-content");
 const acceptCookie = document.querySelector("#accepter");
 const denyCookie = document.querySelector("#afvis");
 const cookies = document.querySelector(".cookie-card");
 
-burger.addEventListener("click", showBurger);
 acceptCookie.addEventListener("click", fjernCookie);
 denyCookie.addEventListener("click", fjernCookie);
-
-function showBurger() {
-  console.log("Hej burger");
-  burger.classList.toggle("open");
-  burgerContent.classList.toggle("open");
-}
 
 function fjernCookie() {
   /*  console.log("hej"); */
